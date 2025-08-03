a79eda1a6fad1e38d22434654b302dce4d59a13f
MateuszDlu <mateuszdluzniewski02@gmail.com>
2025-08-03 14:08:55 +0200

    some design changes and category creation implemented

--- scripts/category.js
+++ scripts/category.js
@@ -5,10 +5,10 @@ function renderCategoriesList(){
   let categoriesListHTML = '';
 
   categoriesList.forEach((categorieObj, index) => {
-    const {name} = categorieObj;
+    const {categoryName} = categorieObj;
     const html = `<div class="category-container">
       <div class="categorie-title">
-        <span class="title-text">${name}</span>
+        <span class="title-text">${categoryName}</span>
         <div class="buttons-container">
           <button class="category-delete-button">X</button>
           <button class="category-modify-button">M</button>
@@ -18,18 +18,34 @@ function renderCategoriesList(){
     </div>`
     categoriesListHTML += html;
   });
-  document.querySelector('.js-categories-container').innerHTML = categoriesListHTML + `<button class="category-add-button js-category-add-button">+</button>`;
+  document.querySelector('.js-categories-container').innerHTML = categoriesListHTML;
 }
 
 renderCategoriesList()
 
 document.querySelector('.js-category-add-button').addEventListener('click', () => {
+  document.querySelector('.js-title-input-popup').style.visibility = 'visible';
+  document.querySelector('.js-title-too-long-error').style.visibility = 'hidden';
+  document.querySelector('.js-title-input').value = '';
+})
+
+document.querySelector('.js-title-input-button-confirm').addEventListener('click', () => {
   addCategory();
 })
 
+document.querySelector('.js-title-input-button-deny').addEventListener('click', () => {
+  document.querySelector('.js-title-input-popup').style.visibility = 'hidden';
+  document.querySelector('.js-title-too-long-error').style.visibility = 'hidden';
+})
 
 function addCategory(){
-  const name = ''
-  categoriesList.push({name})
-  renderCategoriesList()
+  const categoryName = document.querySelector('.js-title-input').value;
+  if(categoryName.length >= 25){
+    document.querySelector('.js-title-too-long-error').style.visibility = 'visible';
+  }else{
+    categoriesList.push({categoryName})
+    document.querySelector('.js-title-input-popup').style.visibility = 'hidden';
+    document.querySelector('.js-title-too-long-error').style.visibility = 'hidden';
+    renderCategoriesList()
+  }
 }
